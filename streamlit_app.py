3ded2f74a39adb528a287707385f390b285b1e5b
Sweekrit-B <sweekrit.bhatnagar@gmail.com>
2025-03-04 18:08:21 -0800

    Made G Drive implementation more robust

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,11 +20,15 @@ st.markdown("""
     </style>
     """, unsafe_allow_html=True)
 
+st.title("📈 Get Anomaly Contributions for Time Series Customer Data")
+link_input = st.text_input("Google Drive Link: ")
+# https://drive.google.com/file/d/1T31Y3ch6tESLpwZ4yPWAaUJaI9W95q7T/view?usp=sharing
+
 @st.cache_data
 def run_data_prep(file):
     tqdm.pandas()
-    
-    drive_link = "https://drive.google.com/file/d/1T31Y3ch6tESLpwZ4yPWAaUJaI9W95q7T/view?usp=sharing"
+
+    drive_link = link_input
 
     # Download and read the Parquet file
     df = download_parquet_from_drive_link(drive_link)
@@ -66,70 +70,77 @@ def run_data_prep(file):
 # visitors_devices = pd.read_csv("visitors_devices_contributors.csv").drop(columns="Unnamed: 0").set_index('ds').reset_index()
 # orders_geography = pd.read_csv("orders_geography_contributors.csv").drop(columns="Unnamed: 0").set_index('ds').reset_index()
 # orders_devices = pd.read_csv("orders_devices_contributors.csv").drop(columns="Unnamed: 0").set_index('ds').reset_index()
-all_events_geography_contributions, all_events_devices_contributions, buyers_geography_contributions, buyers_devices_contributions, visitors_geography_contributions, visitors_devices_contributions, orders_geography_contributions, orders_devices_contributions = run_data_prep("al_15day_export.parquet")
 
-inputs_valid = True
+inputs_valid = False
 
-st.title("📈 Get Anomaly Contributions for Time Series Customer Data")
-st.header("Get contributions")
-input = st.selectbox(
-    "Which metric would you like to view?",
-    ("Visitors", "Orders", "Buyers", "All Events"),
-    index=None,
-    placeholder="Select your metric...",
-)
-date = st.date_input("What date do you want to view?", value="2025-01-06", min_value=min(all_events_geography_contributions['ds']), max_value=max(all_events_geography_contributions['ds']))
-
-if not input or date == "YYYY-MM-DD":
-    st.text("Please select all options")
-    inputs_valid = False
-# if date.replace("-", "").isdigit() and input:
-#     date_format = "%Y-%m-%d"
-#     date_obj = datetime.strptime(date, date_format)
-#     start_date = datetime(2025, 1, 6)
-#     end_date = datetime(2025, 1, 21)
-#     if date_obj < start_date or date_obj > end_date:
-#         st.text("Please enter a valid date")
-#     else:
-#         inputs_valid = True
+if link_input:
+    try:
+        all_events_geography_contributions, all_events_devices_contributions, buyers_geography_contributions, buyers_devices_contributions, visitors_geography_contributions, visitors_devices_contributions, orders_geography_contributions, orders_devices_contributions = run_data_prep("al_15day_export.parquet")
+        inputs_valid = True
+    except:
+        st.markdown("Please enter a valid link.")
+        inputs_valid = False
 
 if inputs_valid:
-    if input.lower() == "buyers":
-        st.header("Buyers")
-        with st.expander("Buyers - geography"):
-            st.dataframe(buyers_geography_contributions, use_container_width=True)
-        with st.expander("Buyers - devices"):
-            st.dataframe(buyers_devices_contributions, use_container_width=True)
-        figure = plot_anomaly_chart_with_hover(get_anomaly_dates(buyers_devices_contributions), \
-            merge(buyers_devices_contributions, buyers_geography_contributions), date, title="Anomaly Contributions to Buyers")
-        st.plotly_chart(figure, use_container_width=True, theme="streamlit", key=None)
-    elif input.lower() == "visitors":
-        st.header("Visitors")
-        with st.expander("Visitors - geography"):
-            st.dataframe(visitors_geography_contributions, use_container_width=True)
-        with st.expander("Visitors - devices"):
-            st.dataframe(visitors_devices_contributions, use_container_width=True)
-        figure = plot_anomaly_chart_with_hover(get_anomaly_dates(visitors_devices_contributions), \
-            merge(visitors_devices_contributions, visitors_geography_contributions), date, title="Anomaly Contributions to Visitors")
-        st.plotly_chart(figure, use_container_width=True, theme="streamlit", key=None)
-    elif input.lower() == "orders":
-        st.header("Orders")
-        with st.expander("Orders - geography"):
-            st.dataframe(orders_geography_contributions, use_container_width=True)
-        with st.expander("Orders - devices"):
-            st.dataframe(orders_devices_contributions, use_container_width=True)
-        figure = plot_anomaly_chart_with_hover(get_anomaly_dates(orders_devices_contributions), \
-            merge(orders_devices_contributions, orders_geography_contributions), date, title="Anomaly Contributions to Orders")
-        st.plotly_chart(figure, use_container_width=True, theme="streamlit", key=None)
-    elif input.lower() == "all events":
-        st.header("All Events")
-        with st.expander("All Events - geography"):
-            st.dataframe(all_events_geography_contributions, use_container_width=True)
-        with st.expander("All Events - devices"):
-            st.dataframe(all_events_devices_contributions, use_container_width=True)
-        figure = plot_anomaly_chart_with_hover(get_anomaly_dates(all_events_devices_contributions), \
-            merge(all_events_devices_contributions, all_events_geography_contributions), date, title="Anomaly Contributions to All Events")
-        st.plotly_chart(figure, use_container_width=True, theme="streamlit", key=None)
+    st.header("Get contributions")
+    input = st.selectbox(
+        "Which metric would you like to view?",
+        ("Visitors", "Orders", "Buyers", "All Events"),
+        index=None,
+        placeholder="Select your metric...",
+    )
+    date = st.date_input("What date do you want to view?", value="2025-01-06", min_value=min(all_events_geography_contributions['ds']), max_value=max(all_events_geography_contributions['ds']))
+
+    if not input or date == "YYYY-MM-DD":
+        st.text("Please select all options")
+        inputs_valid = False
+    # if date.replace("-", "").isdigit() and input:
+    #     date_format = "%Y-%m-%d"
+    #     date_obj = datetime.strptime(date, date_format)
+    #     start_date = datetime(2025, 1, 6)
+    #     end_date = datetime(2025, 1, 21)
+    #     if date_obj < start_date or date_obj > end_date:
+    #         st.text("Please enter a valid date")
+    #     else:
+    #         inputs_valid = True
+
+    if inputs_valid:
+        if input.lower() == "buyers":
+            st.header("Buyers")
+            with st.expander("Buyers - geography"):
+                st.dataframe(buyers_geography_contributions, use_container_width=True)
+            with st.expander("Buyers - devices"):
+                st.dataframe(buyers_devices_contributions, use_container_width=True)
+            figure = plot_anomaly_chart_with_hover(get_anomaly_dates(buyers_devices_contributions), \
+                merge(buyers_devices_contributions, buyers_geography_contributions), date, title="Anomaly Contributions to Buyers")
+            st.plotly_chart(figure, use_container_width=True, theme="streamlit", key=None)
+        elif input.lower() == "visitors":
+            st.header("Visitors")
+            with st.expander("Visitors - geography"):
+                st.dataframe(visitors_geography_contributions, use_container_width=True)
+            with st.expander("Visitors - devices"):
+                st.dataframe(visitors_devices_contributions, use_container_width=True)
+            figure = plot_anomaly_chart_with_hover(get_anomaly_dates(visitors_devices_contributions), \
+                merge(visitors_devices_contributions, visitors_geography_contributions), date, title="Anomaly Contributions to Visitors")
+            st.plotly_chart(figure, use_container_width=True, theme="streamlit", key=None)
+        elif input.lower() == "orders":
+            st.header("Orders")
+            with st.expander("Orders - geography"):
+                st.dataframe(orders_geography_contributions, use_container_width=True)
+            with st.expander("Orders - devices"):
+                st.dataframe(orders_devices_contributions, use_container_width=True)
+            figure = plot_anomaly_chart_with_hover(get_anomaly_dates(orders_devices_contributions), \
+                merge(orders_devices_contributions, orders_geography_contributions), date, title="Anomaly Contributions to Orders")
+            st.plotly_chart(figure, use_container_width=True, theme="streamlit", key=None)
+        elif input.lower() == "all events":
+            st.header("All Events")
+            with st.expander("All Events - geography"):
+                st.dataframe(all_events_geography_contributions, use_container_width=True)
+            with st.expander("All Events - devices"):
+                st.dataframe(all_events_devices_contributions, use_container_width=True)
+            figure = plot_anomaly_chart_with_hover(get_anomaly_dates(all_events_devices_contributions), \
+                merge(all_events_devices_contributions, all_events_geography_contributions), date, title="Anomaly Contributions to All Events")
+            st.plotly_chart(figure, use_container_width=True, theme="streamlit", key=None)
 
 
 
